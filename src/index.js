b6bae83d74ae191cee549ca94d44c73e9db28e1e
nicolemadison <41113514+nicolemadison@users.noreply.github.com>
2024-04-03 05:23:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AboutUs from "./pages/AboutUs";
+import Homepage from "./pages/Homepage";
+import SuccessStories from "./pages/SuccessStories";
+import Footer from "./components/Footer";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <div className="App">
+            <Navbar/>
+            <Routes>
+            <Route path="/" exact element={<Homepage />}></Route>
+            <Route path="/about" element={<AboutUs />}></Route>
+            <Route path="/success-stories" element={<SuccessStories/>} />
+            </Routes>
+            <Footer/>
+        </div>
+        </BrowserRouter>
   </React.StrictMode>
 );
